032492e5b0c40bffda525d6aa64688dbe8b4d683
JiangYu717 <1780552178@qq.com>
2025-08-24 01:32:29 +0800

    Add files via upload

---
+++ studenthou/src/main/java/com/aquaguardian/AquaGuardianApplication.java
@@ -0,0 +1,31 @@
+package com.aquaguardian;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@MapperScan("com.aquaguardian.mapper")
+public class AquaGuardianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquaGuardianApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        
+        return new CorsFilter(source);
+    }
+} 
---
+++ studenthou/src/test/java/org/example/studenthou/StudenthouApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.studenthou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = com.aquaguardian.AquaGuardianApplication.class)
+class StudenthouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
